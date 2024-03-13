c27e682d99afa84c0e0694781f5cb88728666d4f
Nathen Wiley <nzw0037@auburn.edu>
2024-03-12 21:40:16 -0700

    Completed EmojiCounter App

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -3,24 +3,67 @@
 //
 // Copyright © 2023 Auburn University.
 // All Rights Reserved.
-
-
 import SwiftUI
 
-struct ContentView: View {
+struct EmojiCounter: Identifiable {
+    let id = UUID()
+    let emoji: String
+    var count: Int
+}
+
+struct EmojiCounterView: View {
+    @State private var emojiCounters: [EmojiCounter] = [
+        EmojiCounter(emoji: "😂", count: 0),
+        EmojiCounter(emoji: "😍", count: 0),
+        EmojiCounter(emoji: "😊", count: 0),
+        EmojiCounter(emoji: "🤓", count: 0),
+        EmojiCounter(emoji: "😎", count: 0)
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach($emojiCounters) { $emojiCounter in
+                    HStack {
+                        Text(emojiCounter.emoji)
+                            .font(.largeTitle)
+                        Spacer()
+                        Text("Counter: \(emojiCounter.count)")
+                        Spacer()
+                        Button(action: {
+                            emojiCounter.count += 1
+                        }) {
+                            Text("+")
+                                .font(.title)
+                                .foregroundColor(.green)
+                                .padding(5)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .stroke(Color.green, lineWidth: 2)
+                                )
+                        }
+                        Button(action: {
+                            emojiCounter.count -= 1
+                        }) {
+                            Text("-")
+                                .font(.title)
+                                .foregroundColor(.red)
+                                .padding(5)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .stroke(Color.red, lineWidth: 2)
+                                )
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Emoji Counter")
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        EmojiCounterView()
     }
 }
